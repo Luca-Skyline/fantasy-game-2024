01c04ceb9364529a234232c4ec9f2ec745c29c48
MisterNo0ne <miktn07@gmail.com>
2023-12-03 18:35:13 -0700

    Crappy movement

--- _Fantasy Game/Fantasy Game fr/Assets/josephGaming.cs
+++ _Fantasy Game/Fantasy Game fr/Assets/josephGaming.cs
@@ -5,14 +5,50 @@ using UnityEngine;
 public class josephGaming : MonoBehaviour
 {
     // Joseph do what joseph pleases
+    // i stole this entire thing from https://www.youtube.com/watch?v=f473C43s8nE so if something is broken its jeffrey's fault
+
+    public float sensX, sensY;
+    public Transform orientation, camrea;
+    float xRotation, yRotation;
+
+    public float moveSpeed;
+    float horizontalInput, verticalInput;
+    Vector3 moveDirection;
+    Rigidbody rb;
+
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        //VERY IMPORTANT!!! DONT REMOVE
         gaming(69);
     }
 
     void Update()
     {
-        
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        camrea.rotation = Quaternion.Euler(xRotation, yRotation, 0); //move camera
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0); //rotate player
+
+
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
     }
 
     void gaming(int k)
